aa5987f5aaf0a6ec4b7d56c7ad9e7254b6e5fcfa
Kevin Leimkuhler <kevinleimkuhler@gmail.com>
2017-09-06 22:15:48 -0700

    Add excercises up to 1.27

--- chapter-1/1-1-exercises.scm
+++ chapter-1/1-1-exercises.scm
@@ -11,6 +11,11 @@ when run; use the REPL to test
 (define (even? n)
     (= (remainder n 2) 0))
 
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
 ;;; 1.3
 (define (sum-of-squares a b)
   (+ (square a) (square b)))
@@ -116,3 +121,76 @@ when run; use the REPL to test
                         p
                         q
                         (- count 1)))))
+
+;;; Following along with 1.2.6
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+;;; 1.21
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;;; 1.22
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-inexact-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (when (prime? n)
+      (report-prime (- (current-inexact-milliseconds) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+  (if (even? start)
+      (search-for-primes (+ start 1) end)
+      (cond ((< start end) (timed-prime-test start)
+                           (search-for-primes (+ start 2) end)))))
+
+;;; 1.23
+(define (next x)
+  (if (= x 2) 3 (+ x 2)))
+
+;;; 1.27
+(define (mod-expt-equal? n a)
+  (= (remainder (expt a n) n) a))
+
+(define (congruent-modulo? n)
+  (define (cong-mod n a)
+    (cond ((= a n) #t)
+          ((mod-expt-equal? n a) (cong-mod n (+ a 1)))
+          (else #f)))
+  (cong-mod n 1))
+
+;;; 1.27 alt (reworking functions above)
+(define (fermat-test-carmichael n a)
+  (= (expmod a n n) a))
+
+(define (fermat-carmichael n)
+  (define (iter a)
+    (cond ((= a 1) #t)
+          ((not (fermat-test-carmichael n a)) #f)
+          (else (iter (- a 1)))))
+  (iter (- n 1)))
+
