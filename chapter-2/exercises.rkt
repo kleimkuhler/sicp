67fb59fae2870956abe8629756a5ac04b3c05736
Kevin Leimkuhler <kevinleimkuhler@gmail.com>
2017-09-17 22:51:11 -0700

    Add up to exercise 2.6

--- chapter-2/exercises.rkt
+++ chapter-2/exercises.rkt
@@ -3,7 +3,7 @@
 ;; --------------------------------------------------
 ;; Common defines used among exercises
 (define (gcd a b)
-  (if (= b 0)
+  (if (= b 0.0)
       (abs a)
       (gcd b (remainder a b))))
 
@@ -79,3 +79,49 @@
    (* 2 (+ (rect-width r) (rect-height r))))
 (define (rect-area r)
    (* (rect-width r) (rect-height r)))
+
+;; Following along with reading
+(define (cons-proc-dispatch x y)
+  (define (dispatch m)
+    (cond ((= m 0) x)
+          ((= m 1) y)
+          (else (error "Argument not 0 or 1 -- CONS" m))))
+  dispatch)
+
+(define (car-proc-dispatch z) (z 0))
+(define (cdr-proc-dispatch z) (z 1))
+
+;; 2.4
+(define (cons-lambda x y)
+  (lambda (m) (m x y)))
+
+(define (car-lambda z)
+  (z (lambda (p q) p)))
+
+(define (cdr-lambda z)
+  (z (lambda (p q) q)))
+
+;; 2.5
+;; This is essentially prime factorization problem
+(define (largest-power z b)
+  (define (iter z result)
+    (if (= (remainder z b) 0)
+        (iter (/ z b) (+ 1 result))
+        result))
+  (iter z 0))
+
+(define (cons-product a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car-product z)
+  (largest-power z 2))
+
+(define (cdr-product z)
+  (largest-power z 3))
+
+;; 2.6
+(define zero
+  (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
