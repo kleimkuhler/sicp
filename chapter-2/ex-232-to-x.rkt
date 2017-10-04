887ef508cdb832ce547eefc0f300f50388afb8b9
Kevin Leimkuhler <kevinleimkuhler@gmail.com>
2017-10-03 21:51:26 -0700

    Following along with reading

--- chapter-2/ex-232-to-x.rkt
+++ chapter-2/ex-232-to-x.rkt
@@ -78,3 +78,57 @@
 ;; reverse-left since I am building from right to left
 (define (reverse-left-alt sequence)
   (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+;; Following along with reading
+(define (square x) (* x x))
+
+(define (next x)
+  (if (= x 2) 3 (+ x 2)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (flatmap
+                           (lambda (i)
+                             (map (lambda (j) (list i j))
+                                  (enumerate-interval 1 (- i 1))))
+                           (enumerate-interval 1 n)))))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
