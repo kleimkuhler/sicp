b20b7d3541561c28c299400bbc0be62e0e4e359b
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2017-10-23 15:18:43 -0700

    Add up to exercise 2.58

--- chapter-2/ex-53-x.rkt
+++ chapter-2/ex-53-x.rkt
@@ -20,25 +20,36 @@
 (define (variable? a) (symbol? a))
 (define (same-variable? a b)
   (and (variable? a) (variable? b) (eq? a b)))
+
 (define (make-sum a b)
   (cond ((=number? a 0) b)
         ((=number? b 0) a)
         ((and (number? a) (number? b)) (+ a b))
         (else (list '+ a b))))
+(define (sum? a)
+  (and (pair? a) (eq? (car a) '+)))
+(define (addend a) (cadr a))
+
 (define (make-product a b)
   (cond ((or (=number? a 0) (=number? b 0)) 0)
         ((=number? a 1) b)
         ((=number? b 1) a)
         ((and (number? a) (number? b) (* a b)))
         (else (list '* a b))))
-(define (sum? a)
-  (and (pair? a) (eq? (car a) '+)))
-(define (addend a) (cadr a))
-(define (augend a) (caddr a))
 (define (product? a)
   (and (pair? a) (eq? (car a) '*)))
 (define (multiplier a) (cadr a))
-(define (multiplicand a) (caddr a))
+
+;; 2.56
+(define (make-exponentiation a b)
+  (cond ((=number? b 0) 1)
+        ((=number? b 1) a)
+        ((and (number? a) (number? b)) (expt a b))
+        (else (list '** a b))))
+(define (exponentiation? a)
+  (and (pair? a) (eq? (car a) '**)))
+(define (base a) (cadr a))
+(define (exponent a) (caddr a))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -53,5 +64,24 @@
                         (deriv (multiplicand exp) var))
           (make-product (multiplicand exp)
                         (deriv (multiplier exp) var))))
+        ((exponentiation? exp)
+         (make-product
+          (make-product (exponent exp)
+                        (make-exponentiation (base exp)
+                                             (make-sum (exponent exp) -1)))
+          (deriv (base exp) var)))
         (else
          (error "unknown expression type -- DERIV" exp))))
+
+;; 2.57
+(define (augend a)
+  (if (null? (cdddr a))
+      (caddr a)
+      (cons '+ (cddr a))))
+
+(define (multiplicand a)
+  (if (null? (cdddr a))
+      (caddr a)
+      (cons '* (cddr a))))
+
+;; 2.58
