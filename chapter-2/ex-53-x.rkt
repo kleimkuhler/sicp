0c0ace7a67b672b5fc9640eb6b88e31231759236
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2017-10-24 15:16:25 -0700

    Add up to exercise 2.61

--- chapter-2/ex-53-x.rkt
+++ chapter-2/ex-53-x.rkt
@@ -21,24 +21,24 @@
 (define (same-variable? a b)
   (and (variable? a) (variable? b) (eq? a b)))
 
-(define (make-sum a b)
+(define (make-sum-prefix a b)
   (cond ((=number? a 0) b)
         ((=number? b 0) a)
         ((and (number? a) (number? b)) (+ a b))
         (else (list '+ a b))))
-(define (sum? a)
+(define (sum-prefix? a)
   (and (pair? a) (eq? (car a) '+)))
-(define (addend a) (cadr a))
+(define (addend-prefix a) (cadr a))
 
-(define (make-product a b)
+(define (make-product-prefix a b)
   (cond ((or (=number? a 0) (=number? b 0)) 0)
         ((=number? a 1) b)
         ((=number? b 1) a)
         ((and (number? a) (number? b) (* a b)))
         (else (list '* a b))))
-(define (product? a)
+(define (product-prefix? a)
   (and (pair? a) (eq? (car a) '*)))
-(define (multiplier a) (cadr a))
+(define (multiplier-prefix a) (cadr a))
 
 ;; 2.56
 (define (make-exponentiation a b)
@@ -74,14 +74,77 @@
          (error "unknown expression type -- DERIV" exp))))
 
 ;; 2.57
-(define (augend a)
+(define (augend-prefix a)
   (if (null? (cdddr a))
       (caddr a)
       (cons '+ (cddr a))))
 
-(define (multiplicand a)
+(define (multiplicand-prefix a)
   (if (null? (cdddr a))
       (caddr a)
       (cons '* (cddr a))))
 
 ;; 2.58
+(define (make-sum a b)
+  (cond ((=number? a 0) b)
+        ((=number? b 0) a)
+        ((and (number? a) (number? b)) (+ a b))
+        (else (list a '+ b))))
+(define (sum? a)
+  (and (pair? a) (eq? (cadr a) '+)))
+(define (addend a) (car a))
+(define (augend a) (caddr a))
+
+(define (make-product a b)
+  (cond ((or (=number? a 0) (=number? b 0)) 0)
+        ((=number? a 1) b)
+        ((=number? b 1) a)
+        ((and (number? a) (number? b) (* a b)))
+        (else (list a '* b))))
+(define (product? a)
+  (and (pair? a) (eq? (cadr a) '*)))
+(define (multiplier a) (car a))
+(define (multiplicand a) (caddr a))
+
+;; Following along with reading
+(define (element-of-set? a set)
+  (cond ((null? set) #f)
+        ((equal? a (car set)) #t)
+        (else (element-of-set? a (cdr set)))))
+
+(define (adjoin-set a set)
+  (if (element-of-set? a set)
+      set
+      (cons a set)))
+
+(define (intersection-set a b)
+  (cond ((or (null? a) (null? b)) '())
+        ((element-of-set? (car a) b) (cons (car a) (intersection-set (cdr a) b)))
+        (else (intersection-set (cdr a) b))))
+
+;; 2.59
+(define (union-set a b)
+  (if (null? a)
+      b
+      (union-set (cdr a) (adjoin-set (car a) b))))
+
+;; alt that I like!
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (union-set-acc a b)
+  (accumulate adjoin-set b a))
+
+;; 2.60
+;; element-of-set? remains
+;; intersection-set can remain. Duplicates show when given in
+;; first set. Technically the sets are equal whether there are
+;; duplicates or not, so this shouldn't matter
+(define (adjoin-set-dup a set) (cons a set))
+
+(define (union-set-dup a b) (append a b))
+
+;; 2.61
