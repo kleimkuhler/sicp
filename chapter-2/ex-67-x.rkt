0f33533b03af67673e062b52995c91cdf073e5e9
Kevin Leimkuhler <me@kleimkuhler.com>
2017-11-07 23:00:23 -0800

    Add up to exercise 2.70

--- chapter-2/ex-67-x.rkt
+++ chapter-2/ex-67-x.rkt
@@ -100,3 +100,14 @@
   (encode-symbol-alt-1 tree '()))
 
 ;; 2.69
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge pairs)
+  (if (null? (cdr pairs))
+      (car pairs)
+      (successive-merge (adjoin-set (make-code-tree (car pairs)
+                                                    (cadr pairs))
+                                    (cddr pairs)))))
+
+;; 2.70
