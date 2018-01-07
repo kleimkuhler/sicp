8d06d2b4375f049b30afa38c37a48662f7ecb8f0
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-07 12:07:32 -0500

    Fix to compile correctly

--- chapter-2/ex-67-x.rkt
+++ chapter-2/ex-67-x.rkt
@@ -434,26 +434,27 @@
 ;; try: dispatch type transformation in coercion table
 ;;   1) t1->t2
 ;;   2) t2->t1
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags)))
-      (if proc
-          (apply proc (map contents args))
-          (let ((type1 (car type-tags))
-                (type2 (cadr type-tags))
-                (a1 (car args))
-                (a2 (cadr args)))
-            (if (and (= (length args) 2) (not (eq? type1 type2)))
-                (let ((t1->t2 (get-coercion type1 type2))
-                      (t2->t1 (get-coercion type2 type1)))
-                  (cond (t1->t2
-                         (apply-generic op (t1->t2 a1) a2))
-                        (t2->t1
-                         (apply-generic op a1 (t2->t1 a2)))
-                        (else (error "No method for these types"
-                                     (list op type-tags))))))
-              (error "No method for these types"
-                     (list op type-tags)))))))
+
+;; (define (apply-generic op . args)
+;;   (let ((type-tags (map type-tag args)))
+;;     (let ((proc (get op type-tags)))
+;;       (if proc
+;;           (apply proc (map contents args))
+;;           (let ((type1 (car type-tags))
+;;                 (type2 (cadr type-tags))
+;;                 (a1 (car args))
+;;                 (a2 (cadr args)))
+;;             (if (and (= (length args) 2) (not (eq? type1 type2)))
+;;                 (let ((t1->t2 (get-coercion type1 type2))
+;;                       (t2->t1 (get-coercion type2 type1)))
+;;                   (cond (t1->t2
+;;                          (apply-generic op (t1->t2 a1) a2))
+;;                         (t2->t1
+;;                          (apply-generic op a1 (t2->t1 a2)))
+;;                         (else (error "No method for these types"
+;;                                      (list op type-tags))))))
+;;               (error "No method for these types"
+;;                      (list op type-tags)))))))
 
 ;; 2.81
 ;; a). Infinitely loops on arguments of the same type that do not have `op`
@@ -463,5 +464,3 @@
 
 ;; 2.82-2.86 Meetup discuss
 ;; Page 21 notebook
-
-
