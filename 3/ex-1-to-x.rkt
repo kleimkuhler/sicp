0b9495e6ed79659652879f9ac57e077ad065e4ca
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-16 21:24:42 -0800

    Add up through exercise 3.6

--- 3/ex-1-to-x.rkt
+++ 3/ex-1-to-x.rkt
@@ -61,7 +61,7 @@
                     (f m)))))
     mf))
 
-;; 3.3
+;; 3.3 & 3.4
 (define (make-account password balance)
   (define consecutive-attempts 0)
   (define call-the-cops "911 what's your emergency?")
@@ -87,3 +87,63 @@
                    (lambda (x) call-the-cops)
                    (lambda (x) "Incorrect password")))))
   authenticate)
+
+;; Following along with reading
+
+;; (define rand (let ((x random-init))
+;;                (lambda ()
+;;                  (set! x (rand-update x))
+;;                  x)))
+
+;; Monte Carlo method consists of choosing sample experiments at random from a
+;; large set and then making deductions on the basis of the probabilities
+;; estimated from tabulating the results
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1)
+                 trials-passed))))
+  (iter trials 0))
+
+;; Estimate pi (6/pi^2)
+
+;; (define (estimate-pi trials)
+;;   (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+;; (define (cesaro-test)
+;;   (= (gcd (rand) (rand)) 1))
+
+;; 3.5
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (* (random) range))))
+
+(define (in-circle? x y)
+  (<= (+ (expt (- x 5) 2)
+         (expt (- y 7) 2))
+      (expt 3 2)))
+
+(define (estimate-integral P x1 x2 y1 y2 trials)
+  (let ((area (* (abs (- x1 x2)) (abs (- y1 y2))))
+        (radius (/ (abs (- x1 x2)) 2))
+        (experiment (lambda () (P (random-in-range x1 x2)
+                                  (random-in-range y1 y2)))))
+    (/ (* area (monte-carlo trials experiment)) (* radius radius))))
+
+;; 3.6
+
+;; (define rand
+;;   (let ((x random-init))
+;;     (define (dispatch message)
+;;       (cond ((eq? message 'generate)
+;;              (begin (set! x (rand-update x))
+;;                     x))
+;;             ((eq? message 'reset)
+;;              (lambda (val) (set! x val)))
+;;             (else (error "Unknown request: RAND"
+;;                          message))))
+;;     dispatch))
