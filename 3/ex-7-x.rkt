c7678eab8d1332032c266c2a41696233a43977ce
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-24 21:34:03 -0800

    Add up through exercise 3.11

--- 3/ex-7-x.rkt
+++ 3/ex-7-x.rkt
@@ -53,3 +53,40 @@
                     (begin (set! called #t)
                            x)
                     0))))
+
+;; 3.9
+(define (factorial n)
+  (if (= n 1) 1 (* n (factorial (- n 1)))))
+
+(define (factorial n) (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+;; Show environment structure of (factorial 6) for environment model of
+;; evaluation (on page 25 notebook)
+
+;; 3.10
+;; Drawing of figure 3.10 on page 26 of notebook
+;; (let) is syntactic sugar for
+;;   (let ((<var> <exp>)) <body>)
+;;   ((lambda (<var>) <body>) <exp>)
+;; Using a `let` here would introduct an additional environment. This
+;; additional environment points to E1 which points to global
+
+;; 3.11
+;; (define acc (make-account 50))
+;; ((acc 'deposit) 40)
+;; 90
+;; ((acc 'withdraw) 60)
+;; 30
+
+;; The local state for `acc` is kept in the global environment
+;; The local state for `acc2` is kept in the global environment
+
+;; The parts of the environment structure that the two accounts share are the
+;; `make-account` parameters and bodies that they point to. The body of each
+;; account is not evaluated yet but I do think they would both point to the
+;; same binding for internal procedures
