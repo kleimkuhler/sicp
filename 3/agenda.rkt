d72cbcb3407894e8ce94f8389d009f2c9694a68f
Kevin Leimkuhler <me@kleimkuhler.com>
2018-02-17 21:47:47 -0800

    Add rest of agenda implementation

--- 3/agenda.rkt
+++ 3/agenda.rkt
@@ -2,6 +2,8 @@
 
 (require compatibility/mlist)
 
+(require "queue.rkt")
+
 (provide after-delay
          current-time
          make-agenda
@@ -17,7 +19,7 @@
 (define (current-time agenda) (mcar agenda))
 (define (set-current-time! agenda time)
   (set-mcar! agenda time))
-(define (segments agenda) (cdr agenda))
+(define (segments agenda) (mcdr agenda))
 (define (set-segments! agenda segments)
   (set-mcdr! agenda segments))
 (define (first-segment agenda) (mcar (segments agenda)))
@@ -25,13 +27,45 @@
 (define (empty-agenda? agenda) (null? (segments agenda)))
 
 (define (add-to-agenda! time action agenda)
-  #t)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< (segment-time (mcar segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-agenda)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (mcar segments)) time)
+        (insert-queue! (segment-queue (mcar segments))
+                       action)
+        (let ((rest (mcdr segments)))
+          (if (belongs-before? rest)
+              (set-mcdr!
+               segments
+               (mcons (make-new-time-segment time action)
+                      (mcdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (agenda segments)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (mcons (make-new-time-segment time action)
+                segments))
+        (add-to-segments! segments))))
 
 (define (remove-first-agenda-item! agenda)
-  #t)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (when (empty-queue? q)
+      (set-segments! agenda (rest-segments agenda)))))
 
 (define (first-agenda-item agenda)
-  #t)
+  (if (not (empty-agenda? agenda))
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda
+                           (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))
+      (error "FIRST-AGENDA-ITEM: Called with empty agenda")))
 
 (define (after-delay agenda delay action)
   (add-to-agenda! (+ delay (current-time agenda))
