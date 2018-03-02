5c1167aba4a755c04956ed3c070ee98e14666c55
Kevin Leimkuhler <me@kleimkuhler.com>
2018-03-01 21:27:08 -0800

    Add connector extended example

---
+++ 3/celsius-fahrenheit.scm
@@ -0,0 +1,18 @@
+(load "connector.scm")
+
+(define C (make-connector))
+(define F (make-connector))
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+        (v (make-connector))
+        (w (make-connector))
+        (x (make-connector))
+        (y (make-connector)))
+    (multiplier c w u)
+    (multiplier v x u)
+    (adder v y f)
+    (constant 9 w)
+    (constant 5 x)
+    (constant 32 y)
+    'ok))
