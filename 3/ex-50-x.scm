8cbf569e575eb2f1837c51eb957a86f0a674c538
Kevin Leimkuhler <me@kleimkuhler.com>
2018-03-10 22:25:58 -0800

    Add more streams!

--- 3/ex-50-x.scm
+++ 3/ex-50-x.scm
@@ -71,3 +71,111 @@
 
 ;; 3.53
 ;; 2^n
+
+;; 3.54
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define factorials
+  (cons-stream 1 (mul-streams factorials (integers-starting-from 2))))
+
+;; 3.55
+(define (partial-sums s)
+  (cons-stream (stream-car s) (add-streams (stream-cdr s) (partial-sums s))))
+
+;; 3.56
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(else
+	 (let ((s1car (stream-car s1))
+	       (s2car (stream-car s2)))
+	   (cond ((< s1car s2car)
+		  (cons-stream s1car (merge (stream-cdr s1) s2)))
+		 ((< s2car s1car)
+		  (cons-stream s2car (merge s1 (stream-cdr s2))))
+		 (else
+		  (cons-stream s1car (merge (stream-cdr s1)
+					    (stream-cdr s2)))))))))
+
+;; Two merges needed
+
+;; 3.57
+;; expand is the decimal expansion of two the division of two numbers (radix is base)
+
+;; 3.58
+(define (div-streams s1 s2)
+  (stream-map / s1 s2))
+
+(define (integrate-series s)
+  (div-streams s integers))
+
+;; Formulating iterations as stream processes
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+		 (stream-map (lambda (guess)
+			       (sqrt-improve guess x))
+			     guesses)))
+  guesses)
+
+;; pi/4 = 1 - 1/3 + 1/5 - 1/7 + ...
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+	       (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+	(s1 (stream-ref s 1))
+	(s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+			  (+ s0 (* -2 s1) s2)))
+		 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+	       (make-tableau transform
+			     (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+	      (make-tableau transform s)))
+
+(define (stream-limit s tolerance)
+  (if (< (abs (- (stream-ref s 0) (stream-ref s 1))) tolerance)
+      (stream-ref s 1)
+      (stream-limit (stream-cdr s) tolerance)))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+		(stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+		   (interleave s2 (stream-cdr s1)))))
+
+;; 3.67
+(define (all-pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (interleave (stream-map (lambda (x) (list (stream-car s) x))
+			    (stream-cdr t))
+		(stream-map (lambda (x) (list x (stream-car t)))
+			    (stream-cdr s)))
+    (pairs (stream-cdr s) (stream-cdr t)))))
